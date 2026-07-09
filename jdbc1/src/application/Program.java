3eb91d364f9e73c7e6f18d5e0de5d50e2dd8ab51
Caio Ferreira <147638926+CaioFerreira007@users.noreply.github.com>
2026-07-09 12:07:37 -0300

    Consulting dates of MySQL

---
+++ jdbc1/src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import db.DB;
+import db.DbException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnection();
+			st = conn.createStatement();
+		rs = st.executeQuery("select * from department");
+		
+		while(rs.next()) {
+			System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+		}
+			
+		}catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
+	}
+
+}
