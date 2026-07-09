6a06c72c3d6751a46c7f6d81ffeee195a8baa81f
Caio Ferreira <147638926+CaioFerreira007@users.noreply.github.com>
2026-07-09 13:02:26 -0300

    Insert to datas on MySQL

--- jdbc1/src/application/Program.java
+++ jdbc1/src/application/Program.java
@@ -1,25 +1,68 @@
 package application;
 
-import java.sql.Statement;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import db.DB;
 import db.DbException;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
 
 	public static void main(String[] args) {
-	
+		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
 		Connection conn = null;
 		Statement st = null;
 		ResultSet rs = null;
-		
-		try {
+		PreparedStatement pt = null;
+
+		try{
+			conn = DB.getConnection();
+			pt = conn.prepareStatement(
+					"INSERT INTO seller"
+							+ " (Name,Email,BirthDate,BaseSalary,DepartmentId)"
+							+ "VALUES "
+							+ "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			pt.setString(1,"Caio Gustavo");
+			pt.setString(2, "caio@gmail.com");
+			pt.setDate(3, new java.sql.Date(sdf.parse("10/07/2002").getTime()));
+			pt.setDouble(4, 4500.00);
+			pt.setInt(5, 1);
+			
+	// pt = conn.prepareStatement("insert into department (Name) values ('d1'), ('d2')", Statement.RETURN_GENERATED_KEYS);
+
+
+			int rowsAffected = pt.executeUpdate();
+			if(rowsAffected>0){
+
+				ResultSet rs1= pt.getGeneratedKeys();
+				while(rs1.next()){
+					int id = rs1.getInt(1);
+					System.out.println("ID: "+id);
+				}
+
+			}else{
+				System.out.println("No rows affected!");
+			}
+
+
+		}catch(SQLException e){
+			throw new DbException(e.getMessage());
+		}catch (ParseException e) {
+           throw new RuntimeException(e);
+       }
+		finally {
+			DB.closeStatement(pt);
+
+		}
+
+        try {
 			conn = DB.getConnection();
 			st = conn.createStatement();
-		rs = st.executeQuery("select * from department");
+		rs = st.executeQuery("select * from seller");
 		
 		while(rs.next()) {
 			System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
@@ -32,5 +75,8 @@ public class Program {
 			DB.closeStatement(st);
 		}
 	}
+
+
+
 
 }
